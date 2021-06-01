c6636f5fc7437340287b36682befd0acfba291c6
NewCopyPasteMaster <r_dawod@hotmail.de>
2021-06-01 08:30:45 +0200

    Manage flights and passengers

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,52 @@
 function Passengers() {
 
+    function checkFlightCapacity(flightCapacity, passengersNumber) {
+
+        let totalPassengers = 0;
+        let passengers;
+
+        for (passengers of passengersNumber) {
+            totalPassengers += passengers
+        }
+        if (totalPassengers > flightCapacity) {
+            throw new Error("There are to many passengers")
+        }
+
+
+        return totalPassengers;
+    }
+
+
+
+    function distributeAllSeatsToAllPassengers(VIPpassengers, regularPassengers, nrOfFlights, businessSeatsPerFlight,
+        economySeatsPerFlight) {
+
+        let vipPassengersWithBusinessSeats = 0,
+            vipPassengersWithEconomySeats = 0,
+            regularPassengersWithBusinessSeats = 0,
+            regularPassengersWithEconomySeats = 0;
+
+        let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
+        let availableEconemySeats = nrOfFlights * economySeatsPerFlight;
+
+        var vipbusinessConfiguration = {
+            passengers: VIPpassengers,
+            seats: availableBusinessSeats
+        }
+
+
+    }
+
+    function updateConfiguration(configuration, seatsPerFlight) {
+        let passengersWithseats = 0;
+        
+    }while(configuration.passengers > 0){
+
+    }
+
+    return {
+        checkFlightCapacity
+    }
 }
 
+module.exports = Passengers();
