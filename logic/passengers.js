4dde77fe6cbc3d2db5913e863c4e48869a37146d
NewCopyPasteMaster <r_dawod@hotmail.de>
2021-06-09 08:33:26 +0200

    step 2

--- logic/passengers.js
+++ logic/passengers.js
@@ -29,24 +29,73 @@ function Passengers() {
         let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
         let availableEconemySeats = nrOfFlights * economySeatsPerFlight;
 
-        var vipbusinessConfiguration = {
+        var vipBusinessConfiguration = {
             passengers: VIPpassengers,
             seats: availableBusinessSeats
         }
+        vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
+
+        var vipEconomyConfiguration = {
+            passengers: vipBusinessConfiguration.passengers,
+            seats: availableEconemySeats
+        };
+        vipPassengersWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);
 
 
+        var regularBusinessConfiguration = {
+            passengers: regularPassengers,
+            seats: vipBusinessConfiguration.seats
+        };
+        regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
+
+
+        var regularEconomyConfiguration = {
+            passengers: regularEconomyConfiguration.passengers,
+            seats: vipEconomyConfiguration.seats
+        };
+        regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
+
+        return {
+            vipPassengersWithBusinessSeats: vipPassengersWithBusinessSeats,
+            vipPassengersWithEconomySeats: vipPassengersWithEconomySeats,
+            regularPassengersWithBusinessSeats: regularPassengersWithBusinessSeats,
+            regularPassengersWithEconomySeats: regularPassengersWithEconomySeats
+        };
     }
 
     function updateConfiguration(configuration, seatsPerFlight) {
         let passengersWithseats = 0;
-        
-    }while(configuration.passengers > 0){
 
+
+        while (configuration.passengers > 0) {
+            if (configuration.seats > 0) {
+                if (configuration.passengers >= configuration.seats) {
+                    if (configuration.seats > configuration.seatsPerFlight) {
+                        configuration.passengers -= seatsPerFlight;
+                        passengersWithseats += seatsPerFlight;
+                    } else {
+                        configuration.passengers -= configuration.seats;
+                        passengersWithseats += configuration.seats;
+                        configuration.seats = 0;
+                    }
+                } else {
+                    passengersWithseats += configuration.passengers;
+                    configuration.seats -= configuration.passengers,
+                        configuration.passengers = 0;
+                }
+            } else {
+                break;
+            }
+        }
+        return passengersWithseats;
     }
 
     return {
-        checkFlightCapacity
+        checkFlightCapacity,
+        distributeAllSeatsToAllPassengers
     }
+
+
 }
 
 module.exports = Passengers();
--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,17 @@
 function Util() {
 
+    function calculateTotalDistributedPassengers() {
+
+        let totalPassengerAmount
+        new PassengerAmount = distributeAllSeatsToAllPassengers();
+
+        for (totalPassengerAmount of PassengerAmount)
+        totalPassengerAmount += PassengerAmount
+
+        return totalPassengerAmount;
+    }
+
+    return calculateTotalDistributedPassengers;
 }
 
+module.exports = Util();
